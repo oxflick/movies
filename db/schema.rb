2fbf9990cbc4f437c9974c6f0161b0110d6fca77
OxFlick <flick@funkyflick.com>
2014-12-15 20:16:00 -0800

    Link Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216031448) do
+ActiveRecord::Schema.define(version: 20141216035304) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20141216031448) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
