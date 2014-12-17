f7a8d6ae80785e08de2967876245d8cc1f5758a5
OxFlick <flick@funkyflick.com>
2014-12-16 21:33:13 -0800

    Added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :movie
 
+validates :rating, presence: true
+validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "can only be a whole number between 1 and 5"
+  }
+
+
 end
