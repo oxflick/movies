e6451475162501d9a7714ac0dd24dd7378c29522
OxFlick <flick@funkyflick.com>
2015-01-30 15:45:22 -0800

    Set up devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,22 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Devise gem. Remember to change host to the actual app host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.default_url_options = { :host => 'http://funkyflick.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   
+  config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: 'gmail.com',
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["GMAIL_USERNAME"],
+password: ENV["GMAIL_PASSWORD"]
+}
 
 
 end
