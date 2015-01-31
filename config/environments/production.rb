f1c3c10ceb83667428eaa39090cf2f1b2d04dfe2
OxFlick <flick@funkyflick.com>
2015-01-30 23:36:09 -0800

    Changed back declaration in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   Rails.application.routes.default_url_options[:host] = 'funkyflick.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   
