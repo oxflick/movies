1fe4fec4c06310499e7f934361f68a03684e5385
OxFlick <flick@funkyflick.com>
2015-01-30 16:44:43 -0800

    Changed more setting on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'funkyflick.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
