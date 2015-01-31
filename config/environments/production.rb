8d7df38decfc1c3505e0f13178f2c2a6ec473932
OxFlick <flick@funkyflick.com>
2015-01-31 14:15:50 -0800

    changed settings mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,10 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = 'funkyflick.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   
+ 
   config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
@@ -96,6 +97,6 @@ enable_starttls_auto: true,
 user_name: ENV["GMAIL_USERNAME"],
 password: ENV["GMAIL_PASSWORD"]
 }
-
+ 
 
 end
