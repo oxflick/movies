db4f85e1c93159a11b00b7650e8be3997fd5106a
OxFlick <flick@funkyflick.com>
2015-01-31 20:44:47 -0800

    Added false to tsl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ address: "smtp.gmail.com",
 port: 587,
 domain: 'gmail.com',
 authentication: "plain",
-enable_starttls_auto: true,
+enable_starttls_auto: false,
 user_name: ENV["GMAIL_USERNAME"],
 password: ENV["GMAIL_PASSWORD"],
 :openssl_verify_mode => 'none'
